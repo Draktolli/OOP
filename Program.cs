bcf7e104b172f1bee9dce9e91b1720a3ed4f5f55
Tolya <t.koverin@gmail.com>
2020-11-30 17:46:50 +0300

    lab1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Parser parser = new Parser();
+			parser.Parse(parser.ParseReader("TextFile1.INI"));
+			Console.WriteLine(parser.Get<int>("COMMON", "LogNCDM"));
+			Console.WriteLine(parser.Get<double>("NCMD", "SampleRate"));
+			Console.WriteLine(parser.Get<double>("ADC_DEV", "BufferLenSecons"));
+			Console.ReadKey();
+		}
+	}
+}
